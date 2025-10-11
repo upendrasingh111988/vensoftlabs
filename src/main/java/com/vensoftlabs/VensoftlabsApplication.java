4b5a81fd2ba958e3301c6320d29ef3779aec1935
upendra singh <upendra.singh111988@gmail.com>
2025-10-11 20:47:42 +0530

    inital commit

---
+++ src/main/java/com/vensoftlabs/VensoftlabsApplication.java
@@ -0,0 +1,13 @@
+package com.vensoftlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VensoftlabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VensoftlabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vensoftlabs/VensoftlabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vensoftlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VensoftlabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
