18b5e3ce13cabf648ffa95a147d13fc1269516b7
upendra singh <upendra.singh111988@gmail.com>
2025-10-11 22:15:54 +0530

    pages for website

---
+++ src/main/java/com/vensoftlabs/repository/HomeRepository.java
@@ -0,0 +1,9 @@
+package com.vensoftlabs.repository;
+
+import com.vensoftlabs.model.Home;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HomeRepository extends JpaRepository<Home, Long> {
+}
---
+++ src/main/java/com/vensoftlabs/repository/PortfolioRepository.java
@@ -0,0 +1,9 @@
+package com.vensoftlabs.repository;
+
+import com.vensoftlabs.model.Portfolio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+}
